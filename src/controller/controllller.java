e3c29994c03c63106159b0e220cf814d68f46373
Caroline Jarman <carolinerosejarman@gmail.com>
2018-09-10 09:53:31 -0600

    Created project and the gitignore

---
+++ src/controller/controllller.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class controllller {
+	public void start ()
+	{
+		System.out.println("Behind every code is an enigma");
+	}
+//Class names should start with capital letters...
+	//methods are lower case to start and have () attached to them
+	//variables lc describe  part of class
+}
---
+++ src/controller/runnnner.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class runnnner {
+	public static void main (String [] args)
+	{
+		controllller app = new controllller ();
+		app.start();
+	}
+	
+}
